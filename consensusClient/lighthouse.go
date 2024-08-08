25b02c76ef9bd07945c835a6f4b9b18130430a14
Swanny <swanny@sw4n.dev>
2024-08-08 13:10:30 -0400

    fix: top level resources need unique names for replicas

--- consensusClient/lighthouse.go
+++ consensusClient/lighthouse.go
@@ -35,7 +35,7 @@ func NewLighthouseComponent(ctx *pulumi.Context, name string, args *ConsensusCli
 	}
 
 	component := &ConsensusClientComponent{}
-	err := ctx.RegisterComponentResource(fmt.Sprintf("custom:componenet:ConsensusClient:%s", args.Client), name, component, opts...)
+	err := ctx.RegisterComponentResource(fmt.Sprintf("custom:componenet:ConsensusClient:%s", args.Name), name, component, opts...)
 	if err != nil {
 		return nil, err
 	}
